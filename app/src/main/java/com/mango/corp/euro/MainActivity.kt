6b847b7dd2fcc38f2adfabb69d3e7f402223112c
Edu <etachott@student.42sp.org.br>
2025-05-13 19:29:50 -0300

    Remove unused code

--- app/src/main/java/com/mango/corp/euro/MainActivity.kt
+++ app/src/main/java/com/mango/corp/euro/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
