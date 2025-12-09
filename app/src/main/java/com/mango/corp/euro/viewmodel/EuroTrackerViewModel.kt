c59a7830eef2e30481ec361e44bd370522d9ac8c
Eduardo Tachotte <bfwaend@gmail.com>
2025-12-08 22:32:56 -0300

    Improve retrofit usage on VM

--- app/src/main/java/com/mango/corp/euro/viewmodel/EuroTrackerViewModel.kt
+++ app/src/main/java/com/mango/corp/euro/viewmodel/EuroTrackerViewModel.kt
@@ -5,43 +5,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mango.corp.euro.network.RetrofitInstance
 import kotlinx.coroutines.launch
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
-import retrofit2.converter.kotlinx.serialization.asConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
-
-@Serializable
-internal data class ExchangeRateResponse(
-    val amount: Double,
-    val base: String,
-    val date: String,
-    val rates: Map<String, Double>
-)
-
-internal interface ExchangeRateService {
-    @GET("/latest")
-    suspend fun getExchangeRate(
-        @Query("from") from: String,
-        @Query("to") to: String = "BRL"
-    ): ExchangeRateResponse
-}
 
 class EuroTrackerViewModel : ViewModel() {
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(API_URL)
-        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-        .build()
-
     var rate by mutableStateOf("")
     var loading by mutableStateOf(true)
     var error by mutableStateOf<String?>(null)
 
     fun getExchangeRates() {
-        val dataSource = retrofit.create(ExchangeRateService::class.java)
+        val dataSource = RetrofitInstance.exchangeRateService
+
         loading = true
         viewModelScope.launch {
             try {
@@ -54,9 +28,5 @@ class EuroTrackerViewModel : ViewModel() {
                 loading = false
             }
         }
-    }
-
-    companion object {
-        const val API_URL = "https://api.frankfurter.app/"
     }
 }
